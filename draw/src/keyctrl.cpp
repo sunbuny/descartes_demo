6560ba4c0a18e55cc1e707d6e38eeaa940c29581
sunbin <sunbuny@outlook.com>
2017-06-18 15:16:24 +0800

    add key control node

--- draw/src/keyctrl.cpp
+++ draw/src/keyctrl.cpp
@@ -8,14 +8,14 @@
 #include <stdio.h>
 #include <moveit/move_group_interface/move_group.h>
 
-#define KEYCODE_R 0x61
-#define KEYCODE_L 0x64
-#define KEYCODE_U 0x77
-#define KEYCODE_D 0x73
+#define KEYCODE_R 0x61  //a
+#define KEYCODE_L 0x64  //d
+#define KEYCODE_U 0x77 //w
+#define KEYCODE_D 0x73 //s
 #define KEYCODE_Q 0x71
 
-int kfd = 0;
-struct termios cooked, raw;
+//int kfd = 0;
+//struct termios cooked, raw;
 
 class KeyctrlRobot {
 public:
@@ -38,27 +38,40 @@ KeyctrlRobot::KeyctrlRobot() {
 KeyctrlRobot::~KeyctrlRobot() {
 
 }
-int getkey() {
-    int character;
-    struct termios orig_term_attr;
-    struct termios new_term_attr;
+//int getkey() {
+//    int character;
+//    struct termios orig_term_attr;
+//    struct termios new_term_attr;
+//
+//    /* set the terminal to raw mode */
+//    tcgetattr(fileno(stdin), &orig_term_attr);
+//    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
+//    new_term_attr.c_lflag &= ~(ECHO|ICANON);
+//    new_term_attr.c_cc[VTIME] = 0;
+//    new_term_attr.c_cc[VMIN] = 0;
+//    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+//
+//    /* read a character from the stdin stream without blocking */
+//    /*   returns EOF (-1) if no character is available */
+//    character = fgetc(stdin);
+//
+//    /* restore the original terminal attributes */
+//    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
+//
+//    return character;
+//}
+int getkey()
+{
+    static struct termios oldt, newt;
+    tcgetattr( STDIN_FILENO, &oldt);           // save old settings
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON);                 // disable buffering
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
 
-    /* set the terminal to raw mode */
-    tcgetattr(fileno(stdin), &orig_term_attr);
-    memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
-    new_term_attr.c_lflag &= ~(ECHO|ICANON);
-    new_term_attr.c_cc[VTIME] = 0;
-    new_term_attr.c_cc[VMIN] = 0;
-    tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
+    int c = getchar();  // read character (non-blocking)
 
-    /* read a character from the stdin stream without blocking */
-    /*   returns EOF (-1) if no character is available */
-    character = fgetc(stdin);
-
-    /* restore the original terminal attributes */
-    tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
-
-    return character;
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
+    return c;
 }
 void KeyctrlRobot::keyLoop() {
     char c;
@@ -89,12 +102,12 @@ void KeyctrlRobot::keyLoop() {
                 dirty = true;
                 break;
 
-            case KEYCODE_U:
+            case KEYCODE_D:
                 ROS_INFO("X-AXIS -");
                 pose.position.x -=0.2;
                 dirty = true;
                 break;
-            case KEYCODE_D:
+            case KEYCODE_U:
                 ROS_INFO("X-AXIS +");
                 pose.position.x +=0.2;
                 dirty = true;
@@ -123,18 +136,24 @@ void KeyctrlRobot::keyLoop() {
 
     return;
 }
-void quit(int sig)
-{
-   tcsetattr(kfd, TCSANOW, &cooked);
-   ros::shutdown();
-   exit(0);
-}
+//void quit(int sig)
+//{
+//   tcsetattr(kfd, TCSANOW, &cooked);
+//   ros::shutdown();
+//   exit(0);
+//}
 int main(int argc, char** argv){
     ros::init(argc , argv, "key_ctrl");
+    ROS_INFO("------------------key-control-demo------------------");
+    ROS_INFO("-------use W A S D to control-----------------------");
+    ROS_INFO("-------W move alone world x axis 20 cm--------------------");
+    ROS_INFO("-------S move alone world x axis-20 cm--------------------");
+    ROS_INFO("-------A move alone world y axis-20 cm--------------------");
+    ROS_INFO("-------D move alone world y axis 20 cm--------------------");
     ros::AsyncSpinner spinner(1);
     spinner.start();
     KeyctrlRobot keyctrl;
-    signal(SIGINT, quit);
+//    signal(SIGINT, quit);
     keyctrl.keyLoop();
     return 0;
 }
